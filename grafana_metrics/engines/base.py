bd5e039f5ec64da17264ecd4b872f72aa74bf7b3
Evgeniy Titov <falgore88@gmail.com>
2017-06-13 13:31:40 +0300

    config and metrics

--- grafana_metrics/engines/base.py
+++ grafana_metrics/engines/base.py
@@ -4,5 +4,8 @@ from __future__ import unicode_literals
 
 class Engine(object):
 
-    def send(self):
-        pass
+    def send(self, metrics):
+        """
+        :param metrics: MetricData
+        """
+        raise NotImplemented
--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setup(
     author='Evgeniy Titov',
     author_email='falgore88@gmail.com',
     packages=find_packages(),
+    install_requires=[
+        'influxdb==4.1.1',
+        'psutil==5.2.2'
+    ],
     entry_points={
         'console_scripts': [
             'gmetrics = grafana_metrics.main:main',
