f14c3443adef1cf50f041a939ccc3e6e51340e9d
Evgeniy Titov <falgore88@gmail.com>
2017-06-15 19:01:15 +0300

    new version 0.0.28

--- grafana_metrics/metrics/nginx.py
+++ grafana_metrics/metrics/nginx.py
@@ -29,48 +29,46 @@ class Nginx(Metric):
         self.last_read_row_hash = None
 
     def get_row_hash(self, row):
-        return md5(row).hexdigest()
+        return md5(row).hexdigest() if row else ""
 
     def collect(self):
-        try:
-            with open(self.access_log_path) as fh:
-                print self.access_log_path
-                row_generator = reverse_readline(fh)
-                if not self.last_read_row_hash:
-                    print 'set hash'
-                    row = next(row_generator)
-                    print "row = {}".format(row)
-                    self.last_read_row_hash = self.get_row_hash(row)
-                    print "row = {}".format(row)
-                    print 'set hash success'
-                    return []
-                else:
-                    first_row_hash = None
-                    data = defaultdict(int)
-                    while True:
-                        try:
-                            row = next(row_generator)
-                        except StopIteration:
-                            break
-                        row_hash = self.get_row_hash(row)
-                        if not first_row_hash:
-                            first_row_hash = row_hash
-                        if row_hash == self.last_read_row_hash:
-                            break
-                        match = self.status_re.match(row.strip())
-                        if match:
-                            status = int(match.groups()[-1])
-                            data[status] += 1
-                            data['{}xx'.format(unicode(status)[0])] += 1
-                            data['total'] += 1
-                self.last_read_row_hash = first_row_hash
-                if data:
-                    return [MetricData(
-                        name=self.measurement,
-                        tags=self.tags,
-                        fields=dict(data)
-                    )]
-                else:
-                    return []
-        except Exception as e:
-            print e
+        with open(self.access_log_path) as fh:
+            print self.access_log_path
+            row_generator = reverse_readline(fh)
+            if not self.last_read_row_hash:
+                print 'set hash'
+                row = next(row_generator)
+                print "row = {}".format(row)
+                self.last_read_row_hash = self.get_row_hash(row.strip())
+                print "row = {}".format(row)
+                print 'set hash success'
+                return []
+            else:
+                first_row_hash = None
+                data = defaultdict(int)
+                while True:
+                    try:
+                        row = next(row_generator)
+                        row = row.strip()
+                    except StopIteration:
+                        break
+                    row_hash = self.get_row_hash(row)
+                    if not first_row_hash:
+                        first_row_hash = row_hash
+                    if row_hash == self.last_read_row_hash:
+                        break
+                    match = self.status_re.match(row.strip())
+                    if match:
+                        status = int(match.groups()[-1])
+                        data[status] += 1
+                        data['{}xx'.format(unicode(status)[0])] += 1
+                        data['total'] += 1
+            self.last_read_row_hash = first_row_hash
+            if data:
+                return [MetricData(
+                    name=self.measurement,
+                    tags=self.tags,
+                    fields=dict(data)
+                )]
+            else:
+                return []
