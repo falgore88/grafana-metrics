a0f2c121d3c1148961de78d3ad6a5ae83487633a
Evgeniy Titov <falgore88@gmail.com>
2019-01-28 14:14:11 +0300

    new version 0.0.39

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import with_statement, unicode_literals
 
 from setuptools import find_packages, setup
 
-VERSION = "0.0.38"
+VERSION = "0.0.39"
 
 setup(
     name='grafana-metrics',
